bf6af60dc32d24122b3fea78441f3821d061fc8e
wang-ax <3071254175@qq.com>
2020-10-18 23:19:38 +0800

    合并两个二叉树

---
+++ 20201018/merge_two_binary_trees/Solution.java
@@ -0,0 +1,14 @@
+package merge_two_binary_trees;
+
+/**
+ * ClassName Solution
+ * Description TODO
+ * Author 30712
+ * Date 2020-10-18
+ * Time 12:05
+ */
+public class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        
+    }
+}
---
+++ 20201018/merge_two_binary_trees/TreeNode.java
@@ -0,0 +1,17 @@
+package merge_two_binary_trees;
+
+/**
+ * ClassName TreeNode
+ * Description TODO
+ * Author 30712
+ * Date 2020-10-18
+ * Time 12:06
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
