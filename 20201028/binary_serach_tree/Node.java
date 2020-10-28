0c931d830e8de193a0889dd0592bee7d0941c03f
wang-ax <3071254175@qq.com>
2020-10-28 23:06:01 +0800

    二叉搜索树

---
+++ 20201028/binary_serach_tree/Node.java
@@ -0,0 +1,18 @@
+package binary_serach_tree;
+
+/**
+ * ClassName Node
+ * Description TODO
+ * Author 30712
+ * Date 2020-10-28
+ * Time 20:02
+ */
+public class Node {
+    public Integer key;
+    public Node left;
+    public  Node right;
+
+    public Node(Integer key) {
+        this.key = key;
+    }
+}
