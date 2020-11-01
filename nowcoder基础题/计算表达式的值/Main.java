b73364c35cd7b7673b0024623ec85c712e10f086
wang-ax <3071254175@qq.com>
2020-11-01 21:13:37 +0800

    计算表达式的值

---
+++ nowcoder基础题/计算表达式的值/Main.java
@@ -0,0 +1,16 @@
+package 计算表达式的值;
+
+/**
+ * ClassName Main
+ * Description TODO
+ * Author 30712
+ * Date 2020-11-01
+ * Time 10:18
+ */
+public class Main {
+    public static void main(String[] args) {
+        int a =40;
+        int c =212;
+        System.out.println((-8+22)*a-10+c/2);
+    }
+}
