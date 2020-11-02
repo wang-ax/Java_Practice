c02de23fcdd0c76370f15867ae3ab00b6dce9107
wang-ax <3071254175@qq.com>
2020-11-02 23:24:02 +0800

    你能活多少秒

---
+++ nowcoder基础题/p22_你能活多少秒/Main.java
@@ -0,0 +1,18 @@
+package p22_你能活多少秒;
+
+import java.util.Scanner;
+
+/**
+ * ClassName Main
+ * Description TODO
+ * Author 30712
+ * Date 2020-11-02
+ * Time 21:53
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner  = new Scanner(System.in);
+        int age = scanner.nextInt();
+        System.out.println(age*31560000);
+    }
+}
