f3806404f6f45ce9c30fd3f3673d4690e8edf929
wang-ax <3071254175@qq.com>
2020-12-11 14:47:55 +0800

    example

---
+++ 20201211/test/Test1.java
@@ -0,0 +1,24 @@
+package day15.test;
+
+/**
+ * ClassName day15.test
+ * Description TODO
+ * Author 30712
+ * Date 2020-12-11
+ * Time 13:35
+ */
+public class Test1 {
+    private float f = 1.0f;
+    int m =12;
+    static  int n = 1;
+
+    public static void main(String[] args) {
+        Test1 t =new Test1();
+        System.out.println(Test1.n);
+    }
+    /*public static void main(String[] args) {
+        String a = args[1];
+        String b = args[2];
+        String c= args[3];
+    }*/
+}
