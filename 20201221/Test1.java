6d5b7c011b49be7d41a9ee5d7a176f1a782a0b7e
wang-ax <3071254175@qq.com>
2020-12-25 23:42:02 +0800

    Test

---
+++ 20201221/Test1.java
@@ -0,0 +1,13 @@
+/**
+ * ClassName PACKAGE_NAME
+ * Description TODO
+ * Author 30712
+ * Date 2020-12-21
+ * Time 18:32
+ */
+public class Test1 {
+        static boolean Paddy;
+        public static void main(String args[]){
+            System.out.println(Paddy);//默认为false
+        }
+}
