4d5d641a9c7b7fac93ed145386fc430605995e0d
wang-ax <3071254175@qq.com>
2021-01-13 21:27:25 +0800

    线程池

---
+++ MultiThread/test7/ThreadPoolExecutorTest.java
@@ -0,0 +1,64 @@
+package test7;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * ClassName test7
+ * Description TODO
+ * Author 30712
+ * Date 2021-01-13
+ * Time 20:47
+ */
+public class ThreadPoolExecutorTest {
+    public static void main(String[] args) {
+        //new 线程池
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(
+                5,//核心线程数——正式员工
+                10,//最大线程数——正式员工+临时员工
+                60,
+                TimeUnit.SECONDS,//idle线程的空闲时间：临时工最大的存活时间，超过时间就解雇
+                new LinkedBlockingQueue<>(),//阻塞队列：任务存放的地方——快递仓库
+                new ThreadFactory() {
+                    @Override
+                    public Thread newThread(Runnable r) {//线程池中定义的任务类r
+                        return new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                System.out.println(Thread.currentThread().getName()+"开始执行了");
+                                //r对象是线程池内部封装过的工作任务类（Worker），会一直循环等待的方式从阻塞队列中取任务来执行
+                                 r.run();
+                            }
+                        });
+                    }
+                },//创建线程的工厂类：线程池创建线程时，调用该工厂的方法创建线程——招聘员工的标准
+                new ThreadPoolExecutor.AbortPolicy()
+                /**
+                 * 4种拒绝策略
+                 * 拒绝策略：达到最大线程数且阻塞队列已满，采取的拒绝策略
+                 * AbortPolicy：直接抛异常RejectedExecutionException(不提供hander参数的默认策略)
+                 * CallerRunsPolicy：谁（某个线程）交给我（线程池）任务，我拒绝执行，由谁自己执行
+                 * DiscardPolicy：交给我的任务，直接丢掉
+                 * DiscardOldestPolicy：丢弃阻塞队列中最旧的任务
+                 */
+        );
+        //线程池创建以后，只要有任务就自动执行
+        for (int i =0;i< 20;i++){//20个任务
+            //线程池执行任务：execute、submit(提交执行一个任务)
+            final  int j =i;
+            pool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    //System.out.println(j);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+}
