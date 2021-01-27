c8423e7c1eda7f366246d55274e8ae716fa92f6c
wang-ax <3071254175@qq.com>
2021-01-27 22:52:33 +0800

    记票统计

---
+++ 刷题/计票统计/Main.java
@@ -0,0 +1,21 @@
+package day26.计票统计;
+
+import java.util.Scanner;
+
+/**
+ * ClassName day26.计票统计
+ * Description TODO
+ * Author 30712
+ * Date 2021-01-27
+ * Time 19:48
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()){
+            int n = scanner.nextInt();
+            for (int i =0;i< n;i++){
+            }
+        }
+    }
+}
