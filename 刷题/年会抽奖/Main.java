f37643d3d6a419b8507a0a54d3f42036f4384f2e
wang-ax <3071254175@qq.com>
2021-01-29 22:02:55 +0800

    年会抽奖，计算无人抽中的概率

---
+++ 刷题/年会抽奖/Main.java
@@ -0,0 +1,22 @@
+package day29.年会抽奖;
+
+import java.util.Scanner;
+
+/**
+ * ClassName day29.年会抽奖
+ * Description TODO
+ * Author 30712
+ * Date 2021-01-29
+ * Time 9:44
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //有多组输入
+        while (scanner.hasNext()){
+            int n = scanner.nextInt();//参加晚会的人数
+            //无人获奖的概率
+
+        }
+    }
+}
