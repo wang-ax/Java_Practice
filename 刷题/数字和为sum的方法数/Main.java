62211243722f76ea8182a1f3407e24a66930d87f
wang-ax <3071254175@qq.com>
2021-01-29 22:04:17 +0800

    数字和为sum的方法数

---
+++ 刷题/数字和为sum的方法数/Main.java
@@ -0,0 +1,27 @@
+package day29.数字和为sum的方法数;
+
+import java.util.Scanner;
+
+/**
+ * ClassName day29.数字和为sum的方法数
+ * Description TODO
+ * Author 30712
+ * Date 2021-01-29
+ * Time 9:56
+ */
+public class Main {
+    //
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()){
+            int n = scanner.nextInt();
+            int sum = scanner.nextInt();
+            int[] A = new int[n];
+            int count =0;//方案数
+            for (int i =0;i< n;i++){
+                A[i] = scanner.nextInt();
+                //计算和为sum的方案有几种
+            }
+        }
+    }
+}
