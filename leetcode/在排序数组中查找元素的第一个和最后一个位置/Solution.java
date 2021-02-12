49308f2359e525d19785d7d0b375348c56346abf
wang-ax <3071254175@qq.com>
2021-02-12 20:36:31 +0800

    在排序数组中查找元素的第一个和最后一个位置

--- leetcode/在排序数组中查找元素的第一个和最后一个位置/Solution.java
+++ leetcode/在排序数组中查找元素的第一个和最后一个位置/Solution.java
@@ -9,45 +9,73 @@ import java.util.Arrays;
  * Date 2021-02-11
  * Time 10:52
  */
+//要求的时间复杂度为O（log（n））
 public class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        //数组中第一个等于target的位置和第一个大于target的位置减一
-        int leftIndex = binarySearch(nums,target);//利用二分查找寻找数组中第一个大于等于target的下标
-        int rightIndex =binarySearch1(nums,target)-1;//在数组中寻找第一个大于target的下标
-        if (leftIndex <= rightIndex && rightIndex < nums.length && nums[leftIndex] == target){
-            return new int[]{leftIndex,rightIndex};
-        }
-        return new int[]{-1,-1};
-    }
-    //第一个大于等于target的下标
-    public  int binarySearch(int[] nums ,int target){
-      //二分查找
-        int left =0;
+    //二分查找target是否存在,如果存在就返回数组下标，如果不存在就返回-1
+    public int BinarySearch(int[] nums,int target){
+        int left = 0;
         int right = nums.length-1;
         while (left <= right){
-            int mid = (left +right)/2;
+            int mid = (left + right)/2;
+            //mid = left + (right -left)/2;//不会溢出
             if (nums[mid] == target){
                 return mid;
-            }
-            if (nums[mid] > target){
-                right = mid -1;
-            }else {
-                left = mid+1;
+            }else if(nums[mid] > target){
+                left = mid +1;
+            }else if(nums[mid] < target){
+                right = mid-1;
             }
         }
         return -1;
     }
-    //第一个大于target的下标
-    public  int binarySearch1(int[] nums ,int target){
+
+    public int[] searchRange(int[] nums, int target) {
+        int[] result  = new int[2];
+        int leftIndex = getLeft(nums,target);
+        int rightIndex =getRight(nums,target);
+        result[0] =leftIndex;
+        result[1] = rightIndex;
+        return result;
+    }
+    //找左边界
+    public  int getLeft(int[] nums ,int target){
         int left =0;
         int right = nums.length-1;
+        int mid =0;
         while (left <= right){
-            int mid = (left +right)/2;
-            if (nums[mid] > target){
-                right = mid -1;
-            }else {
+             mid = (left+right)/2;
+             if (nums[mid] == target){
+                 right = mid-1;
+             }else if(nums[mid] < target){
+                 left = mid +1;
+             }else if (nums[mid] > target){
+                 right = mid -1;
+             }
+        }
+        //边界情况
+        if (left == nums.length || nums[left] != target ){
+            return -1;
+        }
+        return left;
+    }
+   //找右边界
+    public  int getRight(int[] nums ,int target){//5
+        int left =0;
+        int right = nums.length-1;
+        int mid =0;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (nums[mid] == target){
                 left = mid+1;
+            }else if (nums[mid] < target){
+                left = mid+1;
+            }else if(nums[mid] > target){
+                right = mid -1;
             }
+        }
+        //边界情况
+        if ( right == -1 || nums[right] != target){
+            return -1;
         }
         return right;
     }
