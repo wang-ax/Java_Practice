87d6840c67b83032e60f284861be9530558428ec
wang-ax <3071254175@qq.com>
2021-04-10 21:26:38 +0800

    lock体系学习

---
+++ 20210410/lock体系/Test.java
@@ -0,0 +1,18 @@
+package lock体系;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * ClassName lock体系
+ * Description TODO
+ * Author 30712
+ * Date 2021-04-10
+ * Time 11:14
+ */
+public class Test {
+    private static  int COUNT;
+    public static void main(String[] args) {
+        Lock lock = new ReentrantLock();
+    }
+}
