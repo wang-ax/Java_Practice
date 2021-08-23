f6e3ff8ddf468f095df7e1a598bfc5e74e7cda52
wang-ax <65639372+wang-ax@users.noreply.github.com>
2021-08-23 23:22:22 +0800

    两个线程打印

--- 笔试/有赞/printHelloWorld/Solution.java
+++ 笔试/有赞/printHelloWorld/Solution.java
@@ -8,16 +8,63 @@ package printHelloWorld;
  * Time 10:10
  */
 public class Solution {
-    public String helloWorld (int n) {
+    /**
+     * 两个线程共用一个helloworld实例，一个hello，一个world
+     * @param n
+     * @return
+     * @throws InterruptedException
+     */
+    public String helloWorld (int n) throws InterruptedException {
         // write code here
+
+       /* StringBuilder sb = new StringBuilder();
+        Runnable runnable1 = new Runnable() {
+            @Override
+            public void run() {
+                sb.append("hello");
+            }
+        };
+        Runnable runnable2 = new Runnable() {
+            @Override
+            public void run() {
+                sb.append("world");
+            }
+        };
+        while ( n > 0){
+            Thread thread1 = new Thread(runnable1);
+            thread1.start();
+
+            Thread thread2 = new Thread(runnable2);
+            thread2.start();
+
+            thread2.join();
+            n--;
+        }
+        return sb.toString();*/
+
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i=0;i<n;i++){
-            stringBuilder.append("helloworld");
+        while (n  > 0){
+            Thread thread1 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    stringBuilder.append("hello");
+                }
+            });
+            thread1.start();
+            Thread thread2 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    stringBuilder.append("world");
+                }
+            });
+            thread2.start();
+            thread2.join();
+            n--;
         }
         return stringBuilder.toString();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Solution solution = new Solution();
         int n = 2;
         System.out.println(solution.helloWorld(n));
