3725c07ea7142ef10931caea1d667a86dff5b14e
wang-ax <65639372+wang-ax@users.noreply.github.com>
2021-09-08 22:45:35 +0800

    吸血鬼数

---
+++ 笔试/融360/吸血鬼数/Solution.java
@@ -0,0 +1,14 @@
+package 融360.吸血鬼数;
+
+/**
+ * ClassName 融360.吸血鬼数
+ * Description TODO
+ * Author 30712
+ * Date 2021-09-07
+ * Time 22:27
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
