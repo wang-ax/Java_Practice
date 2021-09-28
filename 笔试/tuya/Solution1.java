c84d2572f0207d83ed934bcf6a7dbefd28155eef
wang-ax <65639372+wang-ax@users.noreply.github.com>
2021-09-28 22:34:05 +0800

    涂鸦

---
+++ 笔试/tuya/Solution1.java
@@ -0,0 +1,31 @@
+package tuya;
+
+import java.util.ArrayList;
+
+/**
+ * ClassName tuya
+ * Description TODO
+ * [["a", "b"],["b", "c"],["b", "e"],["e", "c"],["a", "f"],
+ * ["f", "e"],["a", "c"],["f", "g"],["g", "h"]]
+ *
+ *
+ * Author 30712
+ * Date 2021-09-28
+ * Time 20:16
+ */
+public class Solution1 {
+    public ArrayList<ArrayList<String>> test_triangle (ArrayList<ArrayList<String>> data) {
+        // write code here
+        ArrayList<ArrayList<String>> list = new ArrayList<>();
+        //每遍历一次找出一个符合的三角形就添加到list中，最终返回的是list
+       /* for (int i =0;i<data.size();i++){
+            ArrayList<String> temp = data.get(i);
+            String a = temp.get(0);
+            String b = temp.get(1);
+
+        }*/
+        //找出符合三角形的数组，如何判断符合三角形，三个list中有的数字分别有两个
+
+        return list;
+    }
+}
